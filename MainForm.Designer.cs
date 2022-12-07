3a9d4efa00bfa406771a8b60780a5a14bbd81eab
ihortetiushev <ihor.tetiushev@gmail.com>
2022-12-07 23:44:17 +0200

    populate authors

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,15 +30,23 @@
         {
             this.tabControl = new System.Windows.Forms.TabControl();
             this.allLiteratureTab = new System.Windows.Forms.TabPage();
-            this.categoriesTab = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.categoriesTab = new System.Windows.Forms.TabPage();
+            this.tabAuthors = new System.Windows.Forms.TabPage();
+            this.authorsSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.authorsGridView = new System.Windows.Forms.DataGridView();
             this.tabControl.SuspendLayout();
             this.allLiteratureTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabAuthors.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.authorsSplitContainer)).BeginInit();
+            this.authorsSplitContainer.Panel2.SuspendLayout();
+            this.authorsSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.authorsGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl
@@ -48,10 +56,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl.Controls.Add(this.allLiteratureTab);
             this.tabControl.Controls.Add(this.categoriesTab);
+            this.tabControl.Controls.Add(this.tabAuthors);
             this.tabControl.Location = new System.Drawing.Point(12, 12);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(776, 426);
+            this.tabControl.Size = new System.Drawing.Size(903, 515);
             this.tabControl.TabIndex = 0;
             // 
             // allLiteratureTab
@@ -65,16 +74,6 @@
             this.allLiteratureTab.Text = "All Literature";
             this.allLiteratureTab.UseVisualStyleBackColor = true;
             // 
-            // categoriesTab
-            // 
-            this.categoriesTab.Location = new System.Drawing.Point(4, 25);
-            this.categoriesTab.Name = "categoriesTab";
-            this.categoriesTab.Padding = new System.Windows.Forms.Padding(3);
-            this.categoriesTab.Size = new System.Drawing.Size(768, 397);
-            this.categoriesTab.TabIndex = 1;
-            this.categoriesTab.Text = "Categories";
-            this.categoriesTab.UseVisualStyleBackColor = true;
-            // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -86,7 +85,7 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
             this.splitContainer1.Size = new System.Drawing.Size(762, 391);
-            this.splitContainer1.SplitterDistance = 254;
+            this.splitContainer1.SplitterDistance = 169;
             this.splitContainer1.TabIndex = 0;
             // 
             // dataGridView1
@@ -99,11 +98,58 @@
             this.dataGridView1.Size = new System.Drawing.Size(842, 193);
             this.dataGridView1.TabIndex = 0;
             // 
+            // categoriesTab
+            // 
+            this.categoriesTab.Location = new System.Drawing.Point(4, 25);
+            this.categoriesTab.Name = "categoriesTab";
+            this.categoriesTab.Padding = new System.Windows.Forms.Padding(3);
+            this.categoriesTab.Size = new System.Drawing.Size(768, 397);
+            this.categoriesTab.TabIndex = 1;
+            this.categoriesTab.Text = "Categories";
+            this.categoriesTab.UseVisualStyleBackColor = true;
+            // 
+            // tabAuthors
+            // 
+            this.tabAuthors.Controls.Add(this.authorsSplitContainer);
+            this.tabAuthors.Location = new System.Drawing.Point(4, 25);
+            this.tabAuthors.Name = "tabAuthors";
+            this.tabAuthors.Size = new System.Drawing.Size(895, 486);
+            this.tabAuthors.TabIndex = 2;
+            this.tabAuthors.Text = "Authors";
+            this.tabAuthors.UseVisualStyleBackColor = true;
+            // 
+            // authorsSplitContainer
+            // 
+            this.authorsSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.authorsSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.authorsSplitContainer.Name = "authorsSplitContainer";
+            this.authorsSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // authorsSplitContainer.Panel2
+            // 
+            this.authorsSplitContainer.Panel2.Controls.Add(this.authorsGridView);
+            this.authorsSplitContainer.Size = new System.Drawing.Size(895, 486);
+            this.authorsSplitContainer.SplitterDistance = 181;
+            this.authorsSplitContainer.TabIndex = 0;
+            // 
+            // authorsGridView
+            // 
+            this.authorsGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.authorsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.authorsGridView.Location = new System.Drawing.Point(3, 3);
+            this.authorsGridView.Name = "authorsGridView";
+            this.authorsGridView.RowHeadersWidth = 51;
+            this.authorsGridView.RowTemplate.Height = 24;
+            this.authorsGridView.Size = new System.Drawing.Size(864, 295);
+            this.authorsGridView.TabIndex = 0;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(927, 539);
             this.Controls.Add(this.tabControl);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -115,6 +161,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabAuthors.ResumeLayout(false);
+            this.authorsSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.authorsSplitContainer)).EndInit();
+            this.authorsSplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.authorsGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -126,5 +177,8 @@
         private System.Windows.Forms.TabPage categoriesTab;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabAuthors;
+        private System.Windows.Forms.SplitContainer authorsSplitContainer;
+        private System.Windows.Forms.DataGridView authorsGridView;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -53,11 +53,50 @@ namespace PersonalLibrary
                 dbConnection.Open();
                 List<Author> allAuthors = authorDao.GetAllAuthors();
                 List<Literature> literature = libraryDao.GetAllLiterature();
+                populateAutorGridData(allAuthors);
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Can not open db connection ! " + ex.Message);
             }
+        }
+
+        private void populateAutorGridData(List<Author> authors) 
+        {
+            DataTable authorsTable = createTable(authorsGridView);
+            foreach (Author author in authors)
+            {
+                 authorsTable.LoadDataRow(toAuthorRow(author), true);
+            }
+        }
+
+        private DataTable createTable(System.Windows.Forms.DataGridView dataGrid)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add(new DataColumn("Id", Type.GetType("System.Int32")));
+            table.Columns.Add(new DataColumn("First Name", Type.GetType("System.String")));
+            table.Columns.Add(new DataColumn("Last Name", Type.GetType("System.String")));
+            table.Columns.Add(new DataColumn("Birth Date", Type.GetType("System.DateTime")));
+            table.Columns.Add(new DataColumn("Comment", Type.GetType("System.String")));
+
+            dataGrid.DataSource = table;
+            dataGrid.Columns[0].Width = 50;
+            dataGrid.Columns[1].Width = 150;
+            dataGrid.Columns[2].Width = 150;
+            dataGrid.Columns[3].Width = 70;
+            dataGrid.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            return table;
+        }
+
+        private object[] toAuthorRow(Author author)
+        {
+            object[] values = new object[5];
+            values[0] = author.authorId;
+            values[1] = author.firstName;
+            values[2] = author.lastName;
+            values[3] = author.birthDate;
+            values[4] = author.comment;
+            return values;
         }
     }
 }
