cdeada23587468b7f0bca1e9e7b9847c5f6f2851
ihortetiushev <ihor.tetiushev@gmail.com>
2022-12-16 01:29:33 +0200

    folders structure

--- View/MainForm.cs
+++ View/MainForm.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 using PersonalLibrary.Dao;
 using PersonalLibrary.Models;
-using PersonalLibrary.View;
+
 
 namespace PersonalLibrary.View
 {
